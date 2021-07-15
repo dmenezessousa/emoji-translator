2686250bed2ce32a36c5cb4134b7a1c536c90c92
KheSanh-Code <dmenezessousaa1@gmail.com>
2021-07-15 13:12:04 -0400

    Done

--- main.js
+++ main.js
@@ -18,8 +18,9 @@ buttonSubmit.addEventListener('click', function () {
     let translateID = document.querySelector('#translate');
     //Get the madlib input.
     let madlibID = document.querySelector('#madlib');
-    //Get all  the array/object for search input.
+    //Get the search input.
     let searchID = document.querySelector('#search');
+    //Get the random input.
     let randomID = document.querySelector('#random');
 
     //if nothing is clicked it will display this message.
@@ -42,7 +43,7 @@ buttonSubmit.addEventListener('click', function () {
     }
     //When search radio button is checked it will return an array of objects that each contain an emoji that matches our search criteria
     else if(checkedRadio === searchID){
-        results.innerHTML = '';
+        results.innerHTML = ''; //empty the Element every time it loops.
         let emojiSearch = search(translatorValue);
         for(i = 0; i < emojiSearch.length; i++){
             let newParaElement = document.createElement('p');
@@ -50,21 +51,27 @@ buttonSubmit.addEventListener('click', function () {
             results.appendChild(newParaElement);
         }
     }
+    //If random radio button was checked, display its function to the user.
     else if(checkedRadio === randomID){
-        let functionArray = [newSearch(translatorValue)]; 
-        results.innerHTML = randomElement(functionArray);
-    }
-    
-});
+        //add all the function to an array.
+        let functionArray = [encode(translatorValue),translate(translatorValue),madlib(translatorValue),search(translatorValue)];
 
-function newSearch(String){
-    results.innerHTML = '';
-    let newDiv= document.createElement('div');
-        let emojiSearch = search(String);
-        for(i = 0; i < emojiSearch.length; i++){
-            let newParaElement = document.createElement('p');
-            newParaElement.innerText = emojiSearch[i].symbol;
-            newDiv.appendChild(newParaElement);
-    }
-    return  newDiv;
-}
+        //Variable for the randomElement function.
+        let randomElem = randomElement(functionArray);
+
+        //function to return one of the fucntions in the array above.
+        if(randomElem.length === search(translatorValue).length){
+        results.innerHTML = '';//empty the Element every time it loops.
+
+        //same function used for the search funtion above.
+        let emojiSearch = search(translatorValue);
+            for(i = 0; i < emojiSearch.length; i++){
+                let newParaElement = document.createElement('p');
+                newParaElement.innerText = emojiSearch[i].symbol;
+                results.appendChild(newParaElement);
+            }
+        }else{
+            results.innerText = randomElem; //return a random function from the array above.
+        }   
+    }   
+});
